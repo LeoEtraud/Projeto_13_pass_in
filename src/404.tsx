848b18bf2da1dd39aba15a7c6e4797abcb076fcb
Leonardo Duarte <leocc_etraud@outlook.com>
2025-03-13 13:20:28 -0300

    [feat] Página de error 404

---
+++ src/404.tsx
@@ -0,0 +1,33 @@
+import { Image, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import animation from "../src/assets/animation.gif";
+
+export function NotFound() {
+  return (
+    <VStack
+      spacing={4}
+      align="center"
+      justify="center"
+      minH="80vh" // Ocupa a altura total da viewport
+      textAlign="center"
+      userSelect={"none"} // Desabilita a seleção de texto
+    >
+      <Image
+        objectFit="cover"
+        maxW={{ base: "100%", sm: "400px" }} // Aumentado para 400px em telas maiores
+        src={animation}
+        alt="Animação de página não encontrada"
+      />
+
+      <Text as="h1" fontSize="2xl" fontWeight="bold">
+        Página não encontrada
+      </Text>
+      <Text>
+        Voltar para{" "}
+        <Link to="/attendees" style={{ color: "blue" }}>
+          Dashboard
+        </Link>
+      </Text>
+    </VStack>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,29 @@
-import { Route, Routes } from 'react-router-dom'
-import { AttendeeList } from '../components/attendee-list'
-import { EventList } from '../components/event-list'
-import { CadAttendee } from '../pages/attendees/create'
-import { Login } from '../pages'
-import { DefaultLayout } from '../layouts'
+import { Route, Routes } from "react-router-dom";
+import { AttendeeList } from "../components/attendee-list";
+import { EventList } from "../components/event-list";
+import { CadAttendee } from "../pages/attendees/create";
+import { Login } from "../pages";
+import { DefaultLayout } from "../layouts";
+import { NotFound } from "../404";
 
 export function Router() {
   return (
     /* MINHAS ROTAS */
     <Routes>
+      {/* ROTA - PÁGINA NÃO ENCONTRADA */}
+      <Route path="*" element={<NotFound />} />
       {/* ROTA - PÁGINA DE LOGIN */}
       <Route path="/" element={<Login />} />
 
       {/* ROTA - PÁGINAS DO SITE */}
-      <Route path='/' element={<DefaultLayout />}>
-
+      <Route path="/" element={<DefaultLayout />}>
         {/* ROTA - PÁGINA DE CADASTRO */}
         <Route path="/cad-attendees" element={<CadAttendee />} />
 
         {/* ROTA - TABELAS DE PARTICIPANTES E EVENTOS */}
         <Route path="/attendees" element={<AttendeeList />} />
         <Route path="/events" element={<EventList />} />
-
       </Route>
-
     </Routes>
-  )
+  );
 }
